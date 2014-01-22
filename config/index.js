871819069ba4c44dfc3bf3df76700d37b50594e8
Nevill <nevill.dutt@gmail.com>
2014-01-22 21:06:08 +0800

    Refactor config/index.js

--- config/index.js
+++ config/index.js
@@ -1,36 +1,34 @@
 var nconf = require('nconf');
 
+var defaults = {
+  database: {
+    driver: 'mongodb',
+    url: '' || process.env.MONGOLAB_URI,
+    options: {}
+  },
+  paypal: {
+    host: 'www.sandbox.paypal.com',
+    userId: '' || process.env.PP_UID,
+    password: '' || process.env.PP_PASSWORD,
+    signature: '' || process.env.PP_SIGNATURE,
+    applicationId: '' || process.env.PP_APP_ID,
+  }
+};
+
 var config = {
   development: {
-    paypal: {
-      host: 'www.sandbox.paypal.com',
-      userId: '' || process.env.PP_UID,
-      password: '' || process.env.PP_PASSWORD,
-      signature: '' || process.env.PP_SIGNATURE,
-      applicationId: '' || process.env.PP_APP_ID,
-    }
   },
   test: {
-    paypal: {
-      host: 'www.sandbox.paypal.com',
-      userId: '' || process.env.PP_UID,
-      password: '' || process.env.PP_PASSWORD,
-      signature: '' || process.env.PP_SIGNATURE,
-      applicationId: '' || process.env.PP_APP_ID,
-    }
   },
   production: {
     paypal: {
       host: 'www.paypal.com',
-      userId: '' || process.env.PP_UID,
-      password: '' || process.env.PP_PASSWORD,
-      signature: '' || process.env.PP_SIGNATURE,
-      applicationId: '' || process.env.PP_APP_ID,
     }
   }
 };
 
 module.exports = function() {
-  var env = process.NODE_ENV || 'development';
-  nconf.defaults(config[env]);
+  var env = process.env.NODE_ENV || 'development';
+  nconf.overrides(config[env]);
+  nconf.defaults(defaults);
 };
