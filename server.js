db8953bc3d88ae30ceec69c096fa203aa3a0d273
Nevill <nevill.dutt@gmail.com>
2014-01-23 10:59:28 +0800

    Add mongodb support

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+var nconf = require('nconf');
+var mongoose = require('mongoose');
+
 var app = require('./');
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log('Listening on port', port);
+mongoose.connect(nconf.get('database:url'), function(err) {
+  if (err) {
+    console.error('mongodb error:', err.message);
+  } else {
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+      console.log('Listening on port', port);
+    });
+  }
 });
