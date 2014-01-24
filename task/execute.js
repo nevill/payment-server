b128e6fa2525729685bde2a9b150c1f76da457d7
Nevill <nevill.dutt@gmail.com>
2014-01-24 21:28:11 +0800

    Refactor task/execute.js, extract a new function 'execute'

--- task/execute.js
+++ task/execute.js
@@ -2,12 +2,11 @@
 
 var util = require('util');
 var url = require('url');
+
 var nconf = require('../config');
 var Paypal = require('../lib/paypal');
 var Payment = require('../model').Payment;
 var db = require('../database');
-
-var paypalClient = new Paypal(nconf.get('paypal'));
 
 var debug = function() {};
 if (process.env.NODE_DEBUG) {
@@ -16,50 +15,54 @@ if (process.env.NODE_DEBUG) {
   };
 }
 
+var returnUrl = url.format({
+  protocol: 'https',
+  host: nconf.get('host'),
+  pathname: '/paypal/success'
+});
+
+var cancelUrl = url.format({
+  protocol: 'https',
+  host: nconf.get('host'),
+  pathname: '/paypal/cancel'
+});
+
+var paypalClient = new Paypal(nconf.get('paypal'));
+
+function execute(payment) {
+  debug('Execute payment: %s ...', payment.id);
+  // prepare a payment request body
+  var data = payment.composePayRequestData();
+  data.returnUrl = returnUrl;
+  data.cancelUrl = cancelUrl;
+
+  paypalClient.pay(data, function(err, body) {
+    if (err) {
+      console.error(err.message);
+    } else {
+      debug(util.inspect(body, {
+        depth: null
+      }));
+      var invoice = {
+        payKey: body.payKey,
+        amount: payment.amount,
+        receivers: payment.receivers
+      };
+      payment.execute(invoice, function(err) {
+        if (err) {
+          console.error(err.message);
+        }
+      });
+    }
+  });
+}
+
 db.init(function() {
   Payment.findDues(function(err, payments) {
     if (err) {
       console.error(err.message);
     } else {
-      var returnUrl = url.format({
-        protocol: 'https',
-        host: nconf.get('host'),
-        pathname: '/paypal/success'
-      });
-
-      var cancelUrl = url.format({
-        protocol: 'https',
-        host: nconf.get('host'),
-        pathname: '/paypal/cancel'
-      });
-
-      payments.forEach(function(payment) {
-        debug('Execute payment: %s ...', payment.id);
-        // prepare a payment request body
-        var data = payment.composePayRequestData();
-        data.returnUrl = returnUrl;
-        data.cancelUrl = cancelUrl;
-
-        paypalClient.pay(data, function(err, body) {
-          if (err) {
-            console.error(err.message);
-          } else {
-            debug(util.inspect(body, {
-              depth: null
-            }));
-            var invoice = {
-              payKey: body.payKey,
-              amount: payment.amount,
-              receivers: payment.receivers
-            };
-            payment.execute(invoice, function(err) {
-              if (err) {
-                console.error(err.message);
-              }
-            });
-          }
-        });
-      });
+      payments.forEach(execute);
     }
   });
 });
