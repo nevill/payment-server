3d62d4a0bc7fd0548a628b7c02115c14caba7a64
Nevill <nevill.dutt@gmail.com>
2014-01-25 00:13:43 +0800

    Refactor task/execute.js with the help from package 'async'

--- task/execute.js
+++ task/execute.js
@@ -37,39 +37,47 @@ function execute(payment, next) {
   data.returnUrl = returnUrl;
   data.cancelUrl = cancelUrl;
 
-  paypalClient.pay(data, function(err, body) {
-    if (err) {
-      console.error(err.message);
-      // don't pass the err to `next`
-      // we want other payments get executed
-      next();
-    } else {
-      debug(util.inspect(body, {
-        depth: null
-      }));
+  debug('Request with: %s', util.inspect(data, {
+    depth: null
+  }));
+
+  async.waterfall([
+    function(cb) {
+      paypalClient.pay(data, function(err, body) {
+        debug('Response from Paypal: %s', util.inspect(body, {
+          depth: null
+        }));
+        cb(err, body);
+      });
+    },
+    function(body, cb) {
       var invoice = {
         payKey: body.payKey,
         amount: payment.amount,
         receivers: payment.receivers
       };
-      payment.execute(invoice, function(err) {
-        if (err) {
-          console.error(err.message);
-        }
-        // don't pass the err to `next`
-        // we want other payments get executed
-        next();
-      });
+      payment.execute(invoice, cb);
     }
+  ], function(err) {
+    if (err) {
+      console.error(err.message);
+    }
+    // don't pass the err to `next`
+    // we want other payments get executed
+    process.nextTick(next);
   });
 }
 
 db.init(function() {
-  Payment.findDues(function(err, payments) {
+  async.waterfall([
+    Payment.findDues.bind(Payment),
+    function(payments, cb) {
+      async.each(payments, execute, cb);
+    }
+  ], function(err) {
     if (err) {
       console.error(err.message);
-    } else {
-      async.each(payments, execute, db.disconnect);
     }
+    db.disconnect();
   });
 });
