49794a088490e4d93b4c25629e37c1f81d3ce25f
Nevill <nevill.dutt@gmail.com>
2014-01-24 22:21:47 +0800

    Refactor module 'database', listen to event 'open'

--- database.js
+++ database.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 var nconf = require('./config');
 
+mongoose.connect(nconf.get('database:url'), function(err) {
+  if (err) {
+    console.error('mongodb error:', err.message);
+    process.exit(1);
+  }
+});
+
 var callbacks = [];
-if (mongoose.connection.readyState !== 1) {
-  mongoose.connect(nconf.get('database:url'), function(err) {
-    if (err) {
-      console.error('mongodb error:', err.message);
-      process.exit(1);
-    } else {
-      callbacks.forEach(function(callback) {
-        process.nextTick(callback);
-      });
-    }
+mongoose.connection.on('open', function() {
+  callbacks.forEach(function(callback) {
+    process.nextTick(callback);
   });
-}
+});
 
 exports.init = function(next) {
   if (mongoose.connection.readyState !== 1) {
