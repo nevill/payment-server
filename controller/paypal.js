22563e98f7dbcf643b9810351c67cf8af048a94a
Nevill <nevill.dutt@gmail.com>
2014-01-28 23:55:14 +0800

    Refactor controller paypal, to move IPN verify method into lib/paypal

--- controller/paypal.js
+++ controller/paypal.js
@@ -1,46 +1,11 @@
-var https = require('https');
-var qs = require('querystring');
-var nconf = require('nconf');
-
 exports.ipn = function(req, res) {
-  var verify = function(params, callback) {
-    var body = qs.stringify(params);
-
-    var options = {
-      host: nconf.get('paypal:host'),
-      method: 'POST',
-      path: '/cgi-bin/webscr?cmd=_notify-validate',
-      headers: {
-        'Content-Length': body.length
-      }
-    };
-
-    var httpReq = https.request(options, function(httpResp) {
-      var data = [];
-
-      httpResp.on('data', function(d) {
-        data.push(d);
-      });
-
-      httpResp.on('end', function() {
-        callback(null, data.join(''));
-      });
-    });
-
-    httpReq.on('error', function(e) {
-      callback(e);
-    });
-
-    httpReq.write(body);
-    httpReq.end();
-  };
-
-  verify(req.body, function(err, resp) {
+  var paypalClient = this.app.get('paypalClient');
+  paypalClient.verify(req.body, function(err, resp) {
     if (err) {
       //TODO log the error response
       console.log('err in verify response ===>', err);
     } else {
-      console.log('verify response ===>', resp);
+      console.log('verify response ===>', resp.body);
     }
   });
 
@@ -60,7 +25,6 @@ exports.preapproval = function(req, res) {
     ipnNotificationUrl: data.ipnNotificationUrl,
     memo: data.memo,
   };
-
 
   var paypalClient = this.app.get('paypalClient');
   paypalClient.preapproval(requestObj, function(err, body) {
