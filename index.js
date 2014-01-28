1fca49569d151b2ec38c2a6433f5d4af4519de17
Nevill <nevill.dutt@gmail.com>
2014-01-28 15:53:04 +0800

    Make tests passed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var busboyParser = require('./middleware/busboyParser');
 var loadRoutes = require('./route');
+require('./config');
 
 var app = express();
 app.use(busboyParser({immediate: true}));
